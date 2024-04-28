76a8500344133bb54c3633e63ae6fdce67a198ac
Lungelo Gift Nyasengo <164372730+1lunga@users.noreply.github.com>
2024-04-28 19:52:01 +0200

    Add files via upload

---
+++ classActivity/Activity6.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+df = pd.read_csv("C:/Users/June Gemini/Downloads/dataset - 2020-09-24.csv")
+#display first 5 rows
+print(df.head(5))
+
+#display last 5 rows
+print(df.tail(5))
+
+#display first 3 rows
+print(df.head(3))
+
+#count number of missing entries
+missing_values = df.isnull().sum()
+print(missing_values)
+
+#find and count duplicate rows
+duplicates = df.duplicated().sum()
+print("Number of duplicate entries:", duplicates)
+
+#Ploting histogram for numeric column
+df['Wins'].hist()
+plt.show()
